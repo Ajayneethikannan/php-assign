cba02107d1fb5f7be30a0bd7dc476032704f4f17
Ajayneethikannan <35770004+Ajayneethikannan@users.noreply.github.com>
2018-03-19 09:19:36 +0530

    git

---
+++ dbh.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+$dbServername="192.168.121.187";
+$dbUsername="first_year";
+$dbPassword="first_year";
+$dbName="ajay";
+
+$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
+
+?>
+
+
+
+
